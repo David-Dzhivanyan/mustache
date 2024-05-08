5684bbdf5c96d9b38f9ae72be7478415299553cd
Давид Дживанян <d.david@intervolga.ru>
2024-05-08 20:56:43 +0300

    [main-window] - header - вёрстка

--- src/blocks.04-project/window-header/window-header.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/window-header/window-header.tmpl-specs/base.bemjson.js
@@ -4,9 +4,14 @@ module.exports = [
           {block: 'a', content: [
               {block: 'fi', mods: {icon: 'mustache'}},
             ]},
-          {block: 'a', content: 'mustache.com'},
+          {block: 'a', attrs: {href: ''}, content: 'mustache.com'},
           {block: 'a', content: 'File'},
-          {block: 'a', content: 'Help'},
+          {elem: 'dropdown', cls: 'dropdown', content: [
+              {block: 'a', cls: 'dropdown-toggle', attrs: {'data-bs-toggle': 'dropdown', 'data-bs-offset': '0, 5px'}, content: 'Help'},
+              {cls: 'dropdown-menu', content: [
+                  {block: 'a', mods: {white: true}, content: 'Open info'}
+                ]},
+            ]},
         ]},
       {elem: 'info', content: [
           {elem: 'date', content: 'We applied 567 whinkers 5 May <span class="small"> 2024</span>'},
@@ -14,3 +19,21 @@ module.exports = [
         ]},
     ]}
 ];
+
+// {block: 'event-dropdown', cls: 'dropdown', content: [
+//   {elem: 'title', content: title},
+//   {elem: 'toggle', tag: 'button', cls: 'dropdown-toggle', attrs: {'data-bs-toggle': 'dropdown', 'data-bs-offset': '0, 0'}, content: [
+//       {cls: 'd-flex align-items-center', content: [
+//           {block: 'fi', mods: {icon}},
+//           {cls: 'fw-bolder fs-20 fs-md-30', content: categoryTitle},
+//         ]},
+//     ]},
+//   {elem: 'menu', cls: 'dropdown-menu', content: list.map(({ title, icon }) => [
+//       {elem: 'link', tag: 'button', content: [
+//           {cls: 'd-flex align-items-center', content: [
+//               {block: 'fi', mods: {icon}},
+//               {cls: 'fw-bolder fs-20 fs-md-30', content: title},
+//             ]},
+//         ]},
+//     ])},
+// ]}
