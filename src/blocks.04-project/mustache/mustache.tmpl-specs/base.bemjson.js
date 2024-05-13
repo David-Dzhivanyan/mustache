19b11ddd3cc9e33ba63236b47daa23d3cd24ae14
Dzhivanyan <d.david@intervolga.ru>
2024-05-13 17:44:35 +0300

    [mustache] - ready

--- src/blocks.04-project/mustache/mustache.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/mustache/mustache.tmpl-specs/base.bemjson.js
@@ -2,6 +2,9 @@ module.exports = [
 	{block: 'mustache', content: [
 			{elem: 'bg', content: [
 					{block: 'img', lazy: true, src: './images/mustache/mustache_bg.png'}
-				]}
+				]},
+			{elem: 'gif', content: [
+					{tag: 'img', attrs: { src: './images/mustache/mustache-3d.gif'}}
+				]},
 		]}
 ];
