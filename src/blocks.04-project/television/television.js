1bfad56744ba5b7e602a3fe1e4bad74eb2970dd7
Dzhivanyan <d.david@intervolga.ru>
2024-05-14 07:28:55 +0300

    [television] - вставка усов в canvas

--- src/blocks.04-project/television/television.js
+++ src/blocks.04-project/television/television.js
@@ -6,16 +6,69 @@ class Television {
 		this.block.Television = this;
 		this.canvas = this.block.querySelector('.television__canvas');
 		this.canvasWrapper = this.block.querySelector('.television__tv-wrapper');
+		this.mustacheList = this.block.querySelector('.television__list');
+		this.mustachePlace = this.block.querySelector('.television__frame');
+		this.input = $('.television__input');
+		this.inputWrapper = this.block.querySelector('.television__range');
+		this.downloadBtn = this.block.querySelector('.btn-outline');
 
-		this.initCanvas();
 		this.initSlider();
+		this.initCanvas();
+		this.choosingMustache(this.mustacheList.querySelector('.img'));
+
+		this.mustacheList.addEventListener('click', (event) => this.choosingMustache(event.target));
+		this.inputWrapper.addEventListener('change', this.mustacheSize);
+		this.downloadBtn.addEventListener('click', this.downloadImage)
+
 		this.init();
 	}
 
 	init = () => {
-
 		this.block.classList.add('television_inited');
 	};
+
+	downloadImage = () => {
+		const file = document.querySelector('.window-header__input')
+
+		console.log(file.files)
+		if (file.files.length > 0) {
+			const dataUrl = this.canvas.toDataURL('image/png', 1);
+			const link = document.createElement('a');
+			link.href = dataUrl;
+			link.download = 'mustache.png';
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		}
+	}
+
+	choosingMustache = (target) => {
+		if(target.classList.contains('img')) {
+			this.mustachePlace.innerHTML = `<img class="img" src="${target.src}">`;
+			this.imageInFrame = this.mustachePlace.querySelector('.img');
+
+			this.imageSize = {
+				width: this.imageInFrame.clientWidth,
+				height: this.imageInFrame.clientHeight,
+			}
+
+			this.ionRangeSlider.update({
+				from: 100,
+			});
+		}
+	}
+
+	mustacheSize = () => {
+		if (this.imageInFrame) {
+			this.imageInFrame.style.width = (60 * this.ionRangeSlider.result.from / 100) + 'px';
+			this.imageInFrame.style.height = (30 * this.ionRangeSlider.result.from / 100) + 'px';
+
+			this.imageSize = {
+				width: this.imageInFrame.clientWidth,
+				height: this.imageInFrame.clientHeight,
+			}
+		}
+	}
 
 	initCanvas = () => {
 		const context = this.canvas.getContext('2d');
@@ -30,23 +83,32 @@ class Television {
 					this.canvas.height = this.canvasWrapper.clientHeight;
 				}
 
-				// this.canvas.width = this.canvasWrapper.clientWidth;
-				// this.canvas.height = this.canvasWrapper.clientHeight;
 				context.drawImage(event.detail.img, 0, 0, this.canvas.width, this.canvas.height);
+
+				if (this.imageInFrame) {
+					let canvasWidth = (this.canvas.width / 2) - this.imageSize.width / 2;
+					let canvasHeight = (this.canvas.height / 2) - this.imageSize.height / 2;
+					context.drawImage(this.imageInFrame, canvasWidth, canvasHeight, this.imageSize.width, this.imageSize.height);
+				}
 			}
 
 		})
 	}
 
 	initSlider = () => {
-		$('.television__input').ionRangeSlider({
+		this.input.ionRangeSlider({
 			skin: 'round',
 			hide_min_max: true,
 			hide_from_to: true,
-			min: 10,
-			max: 100,
-			from: 550
+			min: 50,
+			max: 300,
+			from: 550,
+			onChange: () => {
+				this.input[0].dispatchEvent(new CustomEvent('change', {bubbles: true}));
+			}
 		});
+
+		this.ionRangeSlider = this.input.data("ionRangeSlider");
 	}
 	static init() {
 		return Array.from(document.querySelectorAll('.television:not(.television_inited)')).forEach((elem) => new Television(elem));
